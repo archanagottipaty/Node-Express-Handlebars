df17146bbb26fd958904e2a456ce7ababd34ec20
Archana Gottipaty <archana.gottipaty@gmail.com>
2021-01-24 14:46:00 -0800

    adding working final version

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom');
+INSERT INTO burgers (burger_name,devoured) VALUES ('Turkey', true);
+

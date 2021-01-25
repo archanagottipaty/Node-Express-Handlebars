21b34b7e5bcb55ee3a2ab34a9c93ae412180ba60
Archana Gottipaty <archana.gottipaty@gmail.com>
2021-01-24 18:54:15 -0800

    changed line 24 to _

--- config/connection.js
+++ config/connection.js
@@ -21,7 +21,7 @@ const mysql = require('mysql');
 
 
 if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB.URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else {
   connection = mysql.createConnection
 ({
